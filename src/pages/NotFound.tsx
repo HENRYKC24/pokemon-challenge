64ec2112019073e9460030e42dbd9495fffc3000
Henry <henrykc24@gmail.com>
2024-03-28 10:48:50 +0100

    chore: type component

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import PageLayout from '../containers/PageLayout';
 
-const NotFound: React.FC = () => {
+const NotFound: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1>Page not found!</h1>
-      <p>Please make sure the url is correct or check your network connection.</p>
-    </div>
+    <PageLayout>
+      <div>
+        <h1>Page not found!</h1>
+        <p>Please make sure the url is correct or check your network connection.</p>
+      </div>
+    </PageLayout>
   );
 };
 
--- src/pages/SearchPokemon.tsx
+++ src/pages/SearchPokemon.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import PageLayout from '../containers/PageLayout';
 
-const SearchPokemon: React.FC = () => {
+const SearchPokemon: React.FC = (): JSX.Element => {
   return (
-    <div>
-      <h1>Search Pokemon!</h1>
-      <p>This is a search page. Use it to search for pokemon.</p>
-    </div>
+    <PageLayout>
+      <div>
+        <h1>Search Pokemon!</h1>
+        <p>This is a search page. Use it to search for pokemon.</p>
+      </div>
+    </PageLayout>
   );
 };
 
