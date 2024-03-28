22c32884ac3cb59089406240d957c952a5f4f65d
Henry <henrykc24@gmail.com>
2024-03-28 18:40:59 +0100

    feat: add mobile menu

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Socials from '../Socials';
 import css from './footer.module.scss'
+import { Link } from 'react-router-dom';
 
 const Footer: React.FC = (): JSX.Element => {
   return (
     <footer className={css.parent}>
       <Socials height={27} width={27} gap='20px' size={1.5} />
+      <p>@{new Date().getFullYear()} Pokemon Challenge - <Link to="https://henrykc24.github.io/my-portfolio/">Henry KC</Link></p>
     </footer>
   );
 };
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import css from "./header.module.scss";
 import PokemonLogo from "../../assets/images/pokemon_logo.jpeg";
+import Icon from "@mdi/react";
+import { mdiMenu, mdiWindowClose } from "@mdi/js";
 
 import Socials from "../Socials";
 
 const Header: React.FC = (): JSX.Element => {
   const location = useLocation();
+
+  const hamburgerMenuRef: React.RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
+  const desktopRef: React.RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
+  const mobileRef: React.RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
+  const closeBtnRef: React.RefObject<HTMLInputElement> =
+    useRef<HTMLInputElement>(null);
 
   // Function to extract the active tab from the URL pathname
   const getActiveTab: () => {} = (): string => {
@@ -16,6 +27,67 @@ const Header: React.FC = (): JSX.Element => {
     return "";
   };
 
+  useEffect(() => {
+    if (hamburgerMenuRef.current) {
+      hamburgerMenuRef.current.addEventListener("click", () => {
+        if (desktopRef.current && mobileRef.current) {
+          desktopRef.current.style.display = "none";
+          mobileRef.current.style.display = "flex";
+          if (hamburgerMenuRef.current) {
+            hamburgerMenuRef.current.style.display = "none";
+          }
+        }
+      });
+    }
+
+    if (closeBtnRef.current) {
+      closeBtnRef.current.addEventListener("click", () => {
+        if (desktopRef.current && mobileRef.current && hamburgerMenuRef.current) {
+          if (window.innerWidth > 768) {
+            desktopRef.current.style.display = "flex";
+            mobileRef.current.style.display = "none";
+            hamburgerMenuRef.current.style.display = "none";
+          } else {
+            desktopRef.current.style.display = "none";
+            mobileRef.current.style.display = "none";
+            hamburgerMenuRef.current.style.display = "flex";
+          }
+        }
+      });
+    }
+
+    window.addEventListener("resize", () => {
+      const width = window.innerWidth;
+      if (width > 768) {
+        if (desktopRef.current) {
+          desktopRef.current.style.display = "flex";
+          if (hamburgerMenuRef.current) {
+            hamburgerMenuRef.current.style.display = "none";
+          }
+        }
+        // if (mobileRef.current) {
+        //   mobileRef.current.style.display = 'none';
+        //   if (hamburgerMenuRef.current) {
+        //     hamburgerMenuRef.current.style.display = "none";
+        //   }
+        // }
+      } else {
+        if (desktopRef.current) {
+          desktopRef.current.style.display = "none";
+          if (hamburgerMenuRef.current) {
+            hamburgerMenuRef.current.style.display = "flex";
+          }
+        }
+        // if (mobileRef.current) {
+        //   mobileRef.current.style.display = 'flex';
+        //   if (hamburgerMenuRef.current) {
+        //     hamburgerMenuRef.current.style.display = "flex";
+        //   }
+        // }
+      }
+    });
+  }, []);
+
   return (
     <header className={css.header}>
       <nav className={css.nav}>
@@ -24,7 +96,9 @@ const Header: React.FC = (): JSX.Element => {
             <img height={50} width={100} src={PokemonLogo} alt="Pokemon logo" />
           </Link>
         </div>
-        <div className={css.innerDiv}>
+        
+        
+        <div ref={desktopRef} className={css.innerDiv}>
           <ul>
             <li>
               <Link
@@ -42,8 +116,41 @@ const Header: React.FC = (): JSX.Element => {
                 Search
               </Link>
             </li>
+          <Socials height={20} width={20} gap="20px" size={1.2} />
           </ul>
-          <Socials height={15} width={15} gap="20px" size={0.8} />
+        </div>
+
+
+        
+        
+        <div ref={mobileRef} className={css.mobile}>
+          <span ref={closeBtnRef} className={css.closeBtn}>
+            <Icon path={mdiWindowClose} size={1.5} />
+          </span>
+          <ul>
+            <li>
+              <Link
+                className={getActiveTab() === "Home" ? css.active : ""}
+                to="/"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/pokemon-list"
+                className={getActiveTab() === "Search" ? css.active : ""}
+              >
+                Search
+              </Link>
+            </li>
+          <Socials height={20} width={20} gap="20px" size={1.2} />
+          </ul>
+        </div>
+
+
+        <div ref={hamburgerMenuRef} className={css.hamburger}>
+          <Icon path={mdiMenu} size={1.5} />
         </div>
       </nav>
     </header>
