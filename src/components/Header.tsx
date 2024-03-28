fcd2ef1283c8c7b754723abf26874f690ab1626d
Henry <henrykc24@gmail.com>
2024-03-28 13:21:15 +0100

    feat: finish up header implementation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import css from "../styles/header.module.scss";
+import Icon from "@mdi/react";
+import { mdiFacebook, mdiInstagram, mdiTwitter, mdiLinkedin } from "@mdi/js";
+import PokemonLogo from "../assets/images/pokemon_logo.jpeg";
 
 const Header: React.FC = (): JSX.Element => {
+  const location = useLocation();
+
+  // Function to extract the active tab from the URL pathname
+  const getActiveTab: () => {} = (): string => {
+    const path = location.pathname;
+    if (path === "/") return "Home";
+    if (path === "/pokemon-list") return "Search";
+    return "";
+  };
+
   return (
-    <header>
-      <p>Page Header</p>
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <div>
+          <Link to="/">
+            <img height={50} width={100} src={PokemonLogo} alt="Pokemon logo" />
+          </Link>
+        </div>
+        <ul>
+          <li>
+            <Link className={getActiveTab() === 'Home' ? css.active : ''} to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/pokemon-list"  className={getActiveTab() === 'Search' ? css.active : ''}>Search</Link>
+          </li>
+          <li>
+            <Link to="/">
+              <Icon path={mdiFacebook} size={0.8} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <Icon path={mdiTwitter} size={0.8} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <Icon path={mdiInstagram} size={0.8} />
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <Icon path={mdiLinkedin} size={0.8} />
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 };
